7de398ba6ad5a6a0c39a359e482a4e2fd37f7bd4
lilkodh <khaliddrihemdev@gmail.com>
2026-09-02 08:57:14 +0100

    chore: update backend host IP address to 192.168.1.148

--- mobile/src/config/env.ts
+++ mobile/src/config/env.ts
@@ -1,2 +1,2 @@
-export const API_URL = process.env.EXPO_PUBLIC_API_URL || 'http://192.168.170.203:3000/api';
-export const SOCKET_URL = process.env.EXPO_PUBLIC_SOCKET_URL || 'http://192.168.170.203:3000';
+export const API_URL = process.env.EXPO_PUBLIC_API_URL || 'http://192.168.1.148:3000/api';
+export const SOCKET_URL = process.env.EXPO_PUBLIC_SOCKET_URL || 'http://192.168.1.148:3000';
